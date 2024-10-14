8c05c647ab4dcb55b1e5a4dd53f6b7659ec87ede
Agnes <agypeter97@gmail.com>
2024-10-15 00:54:59 +0300

    validate registration and login data

--- laravel-backend/app/Http/Controllers/AuthController.php
+++ laravel-backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validateData = $request->validate([
+            'name'=> 'required|max:60',
+            'email'=> 'email|required|unique:users',
+            'password'=> 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = Hash::make($request->password);
+
+        $user = User::create($validatedData);
+
+        $acessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $acessToken]);
+
+        }
+
+
+        public function login(Request $request){
+            $loginData = $request->validate([
+                'email'=> 'email|required',
+                'password '=>'required'
+            ]);
+
+            if (!auth()->attempt($loginData)) {
+                return response (['message'=> 'Invalid Credentials. Please try again']);
+            }
+
+            $accessToken = auth()->user()->accessToken('authToken')->accessToken;
+            return response (['user'=>auth()->user(), 'access_token'=>$accessToken]);
+        }
 }
