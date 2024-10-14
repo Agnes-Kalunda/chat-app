5559cb193e9cbfc9241a38369806f7ebae903a2c
Agnes <agypeter97@gmail.com>
2024-10-15 00:26:58 +0300

    update Message model with fillable attr. and usr relationship

--- laravel-backend/app/Models/Message.php
+++ laravel-backend/app/Models/Message.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['content', 'user_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
