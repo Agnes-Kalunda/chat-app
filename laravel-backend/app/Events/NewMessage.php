cb13da61e382f51b2065e4419e5c811d308ddf4e
Agnes <agypeter97@gmail.com>
2024-10-17 11:02:12 +0300

    integrated the event with the MessageController to broadcast messages

--- laravel-backend/app/Events/NewMessage.php
+++ laravel-backend/app/Events/NewMessage.php
@@ -2,24 +2,25 @@
 
 namespace App\Events;
 
+use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewMessage
+class NewMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public $message;
+    public function __construct(Message $message)
     {
         //
+        $this->message = $message;
     }
 
     /**
@@ -27,10 +28,8 @@ class NewMessage
      *
      * @return array<int, \Illuminate\Broadcasting\Channel>
      */
-    public function broadcastOn(): array
+    public function broadcastOn()
     {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+        return new Channel('chat.'.$this->message->conversation_id);
     }
 }
