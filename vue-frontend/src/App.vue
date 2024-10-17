8cfb56e34a36a0e77ea69554f75cf47dfcf72039
Agnes <agypeter97@gmail.com>
2024-10-17 17:20:11 +0300

    component to display conversations

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="sidebar">
+      <ConversationList />
+    </div>
+    <div class="main">
+      <template v-if="currentConversation">
+        <div class="chat-header">
+          <h2>{{ getConversationName }}</h2>
+        </div>
+        <MessageList />
+        <MessageInput />
+      </template>
+      <div v-else class="no-conversation">
+        Select a conversation to start chatting
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConversationList from './components/ConversationList.vue'
+import MessageList from './components/MessageList.vue'
+import MessageInput from './components/MessageInput.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ConversationList,
+    MessageList,
+    MessageInput
+  },
+  computed: {
+    currentConversation() {
+      return this.$store.state.currentConversation
+    },
+    getConversationName() {
+      if (!this.currentConversation) return ''
+      
+      if (this.currentConversation.type === 'private') {
+        const otherUser = this.currentConversation.participants.find(
+          p => p.id !== this.$store.state.user.id
+        )
+        return otherUser.name
+      }
+      return this.currentConversation.name
+    }
+  },
+  async created() {
+    if (localStorage.getItem('token')) {
+      //  axios auth header
+      const token = localStorage.getItem('token')
+      this.$axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      
+      // fetch initial data
+      await this.$store.dispatch('fetchConversations')
+    } else {
+      this.$router.push('/login')
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
