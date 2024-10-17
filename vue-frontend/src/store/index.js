9db212a73f49f638b97c814ece1baaf7653dca97
Agnes <agypeter97@gmail.com>
2024-10-17 12:13:02 +0300

    define initial state

---
+++ vue-frontend/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex"
+import axios from 'axios'
+
+
+export default createStore{
+    state:{
+        user:null,
+        conversations: [],
+        currentConversation: null,
+        messages: []
+
+    },
+}
