1ffa4d1ad8a5f9864d3cfb68b0f9da1013d77062
Agnes <agypeter97@gmail.com>
2024-10-17 13:15:08 +0300

    axios to handle API requests for login, fetching conversations,messages,sending newmessages

--- vue-frontend/src/store/index.js
+++ vue-frontend/src/store/index.js
@@ -11,6 +11,7 @@ export default createStore({
 
     },
 
+    // functions to change state
     mutations:{
         SET_user(state, user){
             state.user = user
@@ -28,4 +29,35 @@ export default createStore({
             state.message.unshift(message)
         }
     },
+
+    actions:{
+        async login({ commit }, credentials){
+            const { data } = await axios.post('/api/login', credentials)
+            commit('SET_USER', data.user)
+            localStorage.setItem('token', data.token)
+            axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`
+
+        },
+
+        async fetchConversations({ commit }){
+            const { data } = await axios.get('/api/conversations')
+            commit('SET_CONVERSATIONS', data)
+        },
+
+        async fetchMessages({ commit }){
+            const { data } = await axios.get(`/api/conversations/${conversationId}/messages`)
+            commit('SET_MESSAGES', data)
+        },
+
+        async sendMessage({ commit }, {conversationId, content}){
+            const { data } = await axios.post(`/api/conversations/${conversationId}/messages`, {
+                content,
+                type: 'text'
+            })
+
+            commit('ADD_MESSAGE', data)
+        }
+    }
+
+    
 })
